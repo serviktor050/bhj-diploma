83da2e7cfe6e4d5bc3edf7aeda817628e0e5f321
serviktor050 <mr.s-gagulin@yandex.ru>
2019-10-01 16:33:12 +0300

    Реализован функционал авторизации

--- js/ui/forms/LoginForm.js
+++ js/ui/forms/LoginForm.js
@@ -11,6 +11,13 @@ class LoginForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit( options ) {
-
+    User.login(options, (err, response) => {
+      if (response && (response.success === true)) {
+        this.element.reset();
+        App.setState("user-logged");
+        let loginModal = App.getModal("login");
+        loginModal.close();
+      }
+    })
   }
 }
